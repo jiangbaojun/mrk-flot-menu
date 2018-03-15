46cd7a059bb562f596f8f0c6a95be7cb03128e8f
jiangbaojun <jiangbaojun1001@163.com>
2018-03-15 17:57:43 +0800

    添加info

--- js/mrk-float-menu.js
+++ js/mrk-float-menu.js
@@ -1,3 +1,12 @@
+/* -----------------------------------------------
+/* Author : jiangbaojun
+/* Demo
+/* GitHub : https://github.com/jiangbaojun/mrk-flot-menu.git
+/* How to use? : Check the GitHub README
+/* v1.1
+/* ----------------------------------------------- */
+
+
 /**
  * 浮动展开菜单
  * 数据说明：
